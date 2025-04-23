cd1c1897986a4ec4ac868a10e0b00922bfb6ca07
emericyeou <emeric.yeou@gmail.Com>
2025-04-23 22:03:38 +0200

    fix: pt80

--- src/app/(landing)/ask/page.tsx
+++ src/app/(landing)/ask/page.tsx
@@ -285,7 +285,7 @@ export default function ChatPreview() {
 					</div>
 				) : (
 					/* --- Vue Chat (après le premier message) --- */
-					<div className="mb-40 w-full max-w-2xl flex-grow animate-fade-in space-y-4 overflow-y-auto overflow-x-hidden px-2 py-4 pt-70 md:pt-26">
+					<div className="mb-40 w-full max-w-2xl flex-grow animate-fade-in space-y-4 overflow-y-auto overflow-x-hidden px-2 py-4 pt-80 md:pt-26">
 						{messages.map((msg) => (
 							<div
 								key={msg.id}
