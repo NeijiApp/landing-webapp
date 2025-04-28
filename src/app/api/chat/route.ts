c796360d6f998a7773bf696c38e419fe25734c4e
Alexandre Frede <alexandre.frede.pro@proton.me>
2025-04-28 15:56:36 +0200

    pass to data stream

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,13 +1,15 @@
 import { openai } from "@ai-sdk/openai";
-import { streamText } from "ai";
+import { streamText, type CoreMessage, type Message } from "ai";
+
+type Messages = CoreMessage[] | Omit<Message, "id">[];
 
 export async function POST(request: Request) {
-	const { messages } = await request.json();
+	const { messages }: { messages: Messages } = await request.json();
 
 	const result = streamText({
 		model: openai("gpt-4"),
 		messages,
 	});
 
-	return result.toTextStreamResponse();
+	return result.toDataStreamResponse();
 }
