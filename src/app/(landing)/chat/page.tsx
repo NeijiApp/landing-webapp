f1d1a4c24402dd7bd4956c4e86f8f551a3ab3880
Alexandre Frede <alexandre.frede.pro@proton.me>
2025-04-29 16:01:00 +0200

    add a context

--- src/app/(landing)/chat/page.tsx
+++ src/app/(landing)/chat/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useContext, createContext } from "react";
+
 import { useChat } from "@ai-sdk/react";
 
 import { UserMessage } from "./_components/user-message";
@@ -8,30 +10,66 @@ import { Chat, ChatMessages } from "./_components/chat";
 import { ChatInput } from "./_components/chat-input";
 import { GradientBackground } from "./_components/gradient-background";
 
-export default function ChatPage() {
-	const { messages, input, setInput, handleSubmit } = useChat({
+type ChatContext = {
+	chat: ReturnType<typeof useChat>;
+};
+
+const ChatContext = createContext<ChatContext | null>(null);
+
+function ChatStateProvider({ children }: { children: React.ReactNode }) {
+	const chat = useChat({
 		streamProtocol: "text",
 	});
+
+	return (
+		<ChatContext.Provider value={{ chat }}>{children}</ChatContext.Provider>
+	);
+}
+
+function useChatState() {
+	const state = useContext(ChatContext);
+
+	if (!state)
+		throw new Error(
+			"Invalid usage of useChatState wrap it inside ChatStateProvider",
+		);
+
+	return state;
+}
+
+function ChatLogic() {
+	const {
+		chat: { messages, input, setInput, handleSubmit },
+	} = useChatState();
+
+	return (
+		<Chat>
+			<ChatMessages>
+				{messages.map((message) => {
+					if (message.role === "user") {
+						return (
+							<UserMessage key={message.id}>{message.content}</UserMessage>
+						);
+					}
+
+					return <BotMessage key={message.id}>{message.content}</BotMessage>;
+				})}
+			</ChatMessages>
+			<ChatInput
+				message={input}
+				setMessage={setInput}
+				handleSubmit={handleSubmit}
+			/>
+		</Chat>
+	);
+}
+
+export default function ChatPage() {
 	return (
 		<GradientBackground>
-			<Chat>
-				<ChatMessages>
-					{messages.map((message) => {
-						if (message.role === "user") {
-							return (
-								<UserMessage key={message.id}>{message.content}</UserMessage>
-							);
-						}
-
-						return <BotMessage key={message.id}>{message.content}</BotMessage>;
-					})}
-				</ChatMessages>
-				<ChatInput
-					message={input}
-					setMessage={setInput}
-					handleSubmit={handleSubmit}
-				/>
-			</Chat>
+			<ChatStateProvider>
+				<ChatLogic />
+			</ChatStateProvider>
 		</GradientBackground>
 	);
 }
